804afa9dac4d888a857bde44451c299cbb8536f0
Psofa <lz2842018862@gmail.com>
2024-03-06 22:32:32 +0800

    better private key management(not yet finished)

--- TimeCoinSystem/constants.js
+++ TimeCoinSystem/constants.js
@@ -1,2 +1,2 @@
-export const contractAddress = "";
+export const contractAddress = "http://127.0.0.1:8545/";
 export const contractABI = [];
--- TimeCoinSystem/hardhat.config.js
+++ TimeCoinSystem/hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
+  namedAccounts: {
+    deployer: {
+      default: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+    },
+  },
 };
